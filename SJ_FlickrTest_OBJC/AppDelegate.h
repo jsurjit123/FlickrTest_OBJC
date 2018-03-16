1dc8ddd9df984e0d792da7781923cc2bee789101
Surjit Joshi <surjitjoshi@pu2l1100.corp.knorr-bremse.com>
2018-03-16 16:10:55 +0530

    Initial Commit

---
+++ SJ_FlickrTest_OBJC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJ_FlickrTest_OBJC
+//
+//  Created by Surjit Joshi on 16/03/18.
+//  Copyright © 2018 Surjit Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
