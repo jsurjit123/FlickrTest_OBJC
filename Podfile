60a449454981cade8d7ce4277cb0ab02325df67c
Surjit Joshi <surjitjoshi@pu2l1100.corp.knorr-bremse.com>
2018-03-16 16:06:47 +0530

    Initial Version

---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'SJ_FlickrTest_OBJC' do
+  pod 'AFNetworking'
+  pod 'FlickrKit'
+end
---
+++ Pods/Target Support Files/Pods-SJ_FlickrTest_OBJC/Pods-SJ_FlickrTest_OBJC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SJ_FlickrTest_OBJC : NSObject
+@end
+@implementation PodsDummy_Pods_SJ_FlickrTest_OBJC
+@end
